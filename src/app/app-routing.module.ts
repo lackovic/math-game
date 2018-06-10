8facd80323efed34303e83ddf4a470e3163f8426
Marco Lackovic <marco.lackovic@gmail.com>
2018-06-10 08:49:39 +0300

    Move pages components to pages sub-folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GameComponent } from './game/game.component';
-import { HomeComponent } from './home/home.component';
-import { Error404Component } from './error404/error404.component';
+import { GameComponent } from './pages/game/game.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { Error404Component } from './error404/error404.component';
-import { WaitComponent } from './game/wait/wait.component';
-import { PlayersComponent } from './game/players/players.component';
+import { GameComponent } from './pages/game/game.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './pages/error404/error404.component';
+import { WaitComponent } from './pages/game/wait/wait.component';
+import { PlayersComponent } from './pages/game/players/players.component';
 
 @NgModule({
   declarations: [
