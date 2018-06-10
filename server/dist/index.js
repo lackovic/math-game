0b2c85c5383ba66e8116c19dc2ebdbe980315d2a
Marco Lackovic <marco.lackovic@gmail.com>
2018-06-10 20:11:29 +0300

    Remove unnecessary files

--- server/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var game_server_1 = require("./game-server");
-var app = new game_server_1.GameServer().getApp();
-exports.app = app;
