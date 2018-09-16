9c04f50d3d488edb5f9e05c23b909b675c6abaf1
Marco Lackovic <marco.lackovic@gmail.com>
2018-09-16 18:49:04 +0300

    Add connection test to play component

--- client/src/app/pages/play/play.component.spec.ts
+++ client/src/app/pages/play/play.component.spec.ts
@@ -6,10 +6,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlayComponent } from './play.component';
 import { RouterModule } from '@angular/router';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { SocketService } from '../../services/socket.service';
 
 describe('PlayComponent', () => {
   let component: PlayComponent;
   let fixture: ComponentFixture<PlayComponent>;
+  let socketService: SocketService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,7 +24,8 @@ describe('PlayComponent', () => {
         RouterModule.forRoot([])
       ],
       providers: [
-        { provide: APP_BASE_HREF, useValue: '/' }
+        { provide: APP_BASE_HREF, useValue: '/' },
+        SocketService
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
@@ -32,9 +35,16 @@ describe('PlayComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlayComponent);
     component = fixture.componentInstance;
+    socketService = TestBed.get(SocketService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should try to connect with the server', () => {
+    const connect = spyOn(socketService, 'connect');
+    fixture.detectChanges();
+    expect(connect).toHaveBeenCalled();
   });
 });
