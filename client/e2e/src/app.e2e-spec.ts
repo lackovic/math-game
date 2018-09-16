54c0d3283178c02502c72a548208d8bb65167402
Marco Lackovic <marco.lackovic@gmail.com>
2018-09-17 00:04:54 +0300

    Add 2e2 test for checking that a player can't join a game without a name

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
+  const bec = browser.ExpectedConditions;
 
   beforeEach(() => {
     page = new AppPage();
@@ -11,4 +13,12 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to Math Game!');
   });
+
+  it('should not join the game without a player name', async () => {
+    page.navigateTo();
+    const initialUrl = browser.getCurrentUrl();
+    await page.getJoinButton().click();
+    expect(browser.getCurrentUrl()).toBe(initialUrl);
+  });
+
 });
--- client/e2e/src/app.po.ts
+++ client/e2e/src/app.po.ts
@@ -8,4 +8,9 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getJoinButton() {
+    return element(by.css('button'));
+  }
+
 }
